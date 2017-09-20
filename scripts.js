f3da3b93ad0d0eaedc30a2dc10368212f142b22a
katapo <kata.pochec@gmail.com>
2017-09-20 23:19:07 +0200

    second commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var a = 6;
 var h = 4;
-var triangleArea = a*h/2
+var triangleArea = a * h / 2
 
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
